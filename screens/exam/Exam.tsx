adc8c0e7276cd1a80fd0f90344f9cd0a5e4b0622
sorawit <sorawit@senestia.com>
2024-07-29 16:16:46 +0700

    implement gen exam and list question

--- screens/exam/Exam.tsx
+++ screens/exam/Exam.tsx
@@ -1,7 +1,8 @@
-import {StyleSheet, View} from "react-native";
+import {ScrollView, StyleSheet, View} from "react-native";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../../root.navigation";
 import QuestionAndAnswer from "./components/QuestionAndAnswer";
+import {generateMathExam} from "./utils/generateExam";
 
 const styles = StyleSheet.create({
   container: {
@@ -11,20 +12,30 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     padding: 10,
   },
+  scrollView: {
+    width: '100%',
+  },
 });
 
 type ExamScreenNavigationProp = NativeStackScreenProps<RootStackParamList, 'Exam'>;
 
 export default function ExamScreen() {
+  const exams = generateMathExam(20)
+
   return (
     <View style={styles.container}>
-      <QuestionAndAnswer
-        questionNumber={1}
-        question="What is the capital of Thailand?"
-        choices={["Bangkok", "Hanoi", "Jakarta", "Manila"]}
-        onPress={() => {
-        }}
-      />
+      <ScrollView style={styles.scrollView}>
+        {exams.map((exam) => (
+          <QuestionAndAnswer
+            key={exam.id}
+            questionNumber={exam.id}
+            question={exam.question}
+            choices={exam.choices}
+            onPress={() => {
+            }}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
---
+++ screens/exam/utils/generateExam.ts
@@ -0,0 +1,55 @@
+type ExamType = {
+  id: number;
+  question: string;
+  choices: string[];
+  answerIndex: number;
+}
+
+function getRandomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function generateChoices(correctAnswer: number, answerIndex: number): string[] {
+  const choices = new Set<number>();
+
+  while (choices.size < 4) {
+    if (choices.size === answerIndex) {
+      choices.add(correctAnswer);
+      continue;
+    }
+
+    const choice = correctAnswer + getRandomInt(-50, 50);
+    if (!choices.has(choice)) {
+      choices.add(choice);
+    }
+  }
+
+  return Array.from(choices).map(choice => choice.toString());
+}
+
+function generateQuestion(id: number): ExamType {
+  const num1 = getRandomInt(1, 20);
+  const num2 = getRandomInt(1, 20);
+
+  const question = `${num1} + ${num2}`;
+  const answerIndex = getRandomInt(0, 3);
+  const choices = generateChoices(num1 + num2, answerIndex);
+
+  return {
+    id,
+    question,
+    choices,
+    answerIndex,
+  };
+}
+
+export function generateMathExam(n: number): ExamType[] {
+  const exams: ExamType[] = [];
+
+  for (let i = 1; i <= n; i++) {
+    exams.push(generateQuestion(i));
+  }
+
+  return exams;
+}
