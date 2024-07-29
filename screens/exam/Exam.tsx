36ff3adf143124633e1733e31e4095f8a9b94d99
sorawit <sorawit@senestia.com>
2024-07-29 19:32:25 +0700

    fix disable style

--- screens/exam/Exam.tsx
+++ screens/exam/Exam.tsx
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
   },
   submitFinishButton: {
     borderWidth: 1,
-    color: colors.grey["200"],
+    borderColor: colors.grey["200"],
     width: '100%',
     alignItems: 'center',
     paddingVertical: 5
